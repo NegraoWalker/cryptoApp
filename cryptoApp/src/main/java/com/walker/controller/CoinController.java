c3d33bb0813b1b780680296291edee8a7e1b767e
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-06-01 11:49:09 -0300

    Conclusão da seção 3

--- cryptoApp/src/main/java/com/walker/controller/CoinController.java
+++ cryptoApp/src/main/java/com/walker/controller/CoinController.java
@@ -3,11 +3,15 @@ package com.walker.controller;
 import com.walker.entity.Coin;
 import com.walker.respository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
+
+import static java.lang.System.*;
 
 @RestController
 @RequestMapping(value = "/coin")
@@ -16,15 +20,81 @@ public class CoinController {
     @Autowired
     private CoinRepository coinRepository;
 
+    @Bean
+    public Coin init(){
+        Coin c1 = new Coin();
+        c1.setName("BITCOIN");
+        c1.setPrice(new BigDecimal(100));
+        c1.setQuantity(new BigDecimal(0.0005));
+        c1.setDatetime(new Timestamp(currentTimeMillis()));
+
+        Coin c2 = new Coin();
+        c2.setName("BITCOIN");
+        c2.setPrice(new BigDecimal(250));
+        c2.setQuantity(new BigDecimal(0.0025));
+        c2.setDatetime(new Timestamp(currentTimeMillis()));
+
+        Coin c3 = new Coin();
+        c3.setName("ETHEREUM");
+        c3.setPrice(new BigDecimal(500));
+        c3.setQuantity(new BigDecimal(0.0045));
+        c3.setDatetime(new Timestamp(currentTimeMillis()));
+
+        coinRepository.insert(c1);
+        coinRepository.insert(c2);
+        coinRepository.insert(c3);
+
+        return null;
+    }
+
+
+    @GetMapping()
+    public ResponseEntity get(){
+        return new ResponseEntity<>(coinRepository.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity get(@PathVariable String name) {
+        try {
+            return new ResponseEntity<>(coinRepository.getByName(name), HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @PostMapping()
     public ResponseEntity post(@RequestBody Coin coin) {
 
         try {
-            coin.setDatetime(new Timestamp(System.currentTimeMillis()));
+            coin.setDatetime(new Timestamp(currentTimeMillis()));
             return new ResponseEntity<>(coinRepository.insert(coin), HttpStatus.CREATED);
 
         } catch (Exception error) {
             return new ResponseEntity<>(error.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity put(@RequestBody Coin coin){
+        try {
+            coin.setDatetime(new Timestamp(currentTimeMillis()));
+            return new ResponseEntity<>(coinRepository.update(coin), HttpStatus.OK);
+
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(),HttpStatus.NO_CONTENT);
+        }
+    }
+
+
+
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable int id) {
+        try {
+            return new ResponseEntity<>(coinRepository.remove(id),HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(),HttpStatus.NO_CONTENT);
         }
     }
 
--- cryptoApp/src/main/java/com/walker/respository/CoinRepository.java
+++ cryptoApp/src/main/java/com/walker/respository/CoinRepository.java
@@ -1,15 +1,33 @@
 package com.walker.respository;
 
+import com.walker.dto.CoinTransactionDTO;
 import com.walker.entity.Coin;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 @EnableAutoConfiguration //Pesquisar sobre essa annotation
 public class CoinRepository {
 
     private static String INSERT = "insert into coin (name, price, quantity, datetime) values (?,?,?,?)";
+    private static String SELECT_ALL = "select name, sum(quantity) as quantity from coin group by name";
+
+    private static String SELECT_BY_NAME = "select *from coin where name = ?";
+
+    private static String DELETE =  "delete from coin where id = ?";
+
+    private static String UPDATE = "update coin set name = ?, price = ?, quantity = ? where id = ?";
+
+
+
+
+
 
     private JdbcTemplate jdbcTemplate;
 
@@ -29,5 +47,57 @@ public class CoinRepository {
 
         return coin;
     }
+
+    public Coin update(Coin coin) {
+        Object[] attr = new Object[] {
+                coin.getName(),
+                coin.getPrice(),
+                coin.getQuantity(),
+                coin.getId()
+        };
+
+        jdbcTemplate.update(UPDATE, attr);
+
+        return coin;
+    }
+
+    public List<CoinTransactionDTO> getAll() {
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<CoinTransactionDTO>() {
+            @Override
+            public CoinTransactionDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                CoinTransactionDTO coin = new CoinTransactionDTO();
+                coin.setName(rs.getString("name"));
+                coin.setQuantity(rs.getBigDecimal("quantity"));
+                return coin;
+            }
+        });
+    }
+
+    public List<Coin> getByName(String name){
+        Object[] attr = new Object[] {
+                name
+        };
+        return jdbcTemplate.query(SELECT_BY_NAME, new RowMapper<Coin>() {
+            @Override
+            public Coin mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Coin coin = new Coin();
+                coin.setId(rs.getInt("id"));
+                coin.setName(rs.getString("name"));
+                coin.setPrice(rs.getBigDecimal("price"));
+                coin.setQuantity(rs.getBigDecimal("quantity"));
+                coin.setDatetime(rs.getTimestamp("datetime"));
+
+                return coin;
+            }
+        }, attr);
+    }
+
+    public int remove(int id) {
+        return jdbcTemplate.update(DELETE,id);
+    }
+
+
 
 }
